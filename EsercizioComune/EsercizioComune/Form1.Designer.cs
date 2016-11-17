88c526f54211819267a477d4bcd7cb33a5c55765
enza amabile <enza amabile>
2016-11-17 17:51:07 +0100

    Form vuoto

---
+++ EsercizioComune/EsercizioComune/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace EsercizioComune
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPersona = new System.Windows.Forms.Button();
+            this.cbPersona = new System.Windows.Forms.ComboBox();
+            this.txtPersona = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPersona
+            // 
+            this.btnPersona.Location = new System.Drawing.Point(167, 129);
+            this.btnPersona.Name = "btnPersona";
+            this.btnPersona.Size = new System.Drawing.Size(75, 23);
+            this.btnPersona.TabIndex = 0;
+            this.btnPersona.Text = "Mostra";
+            this.btnPersona.UseVisualStyleBackColor = true;
+            // 
+            // cbPersona
+            // 
+            this.cbPersona.FormattingEnabled = true;
+            this.cbPersona.Location = new System.Drawing.Point(89, 41);
+            this.cbPersona.Name = "cbPersona";
+            this.cbPersona.Size = new System.Drawing.Size(233, 21);
+            this.cbPersona.TabIndex = 1;
+            // 
+            // txtPersona
+            // 
+            this.txtPersona.Location = new System.Drawing.Point(89, 213);
+            this.txtPersona.Name = "txtPersona";
+            this.txtPersona.Size = new System.Drawing.Size(233, 20);
+            this.txtPersona.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 280);
+            this.Controls.Add(this.txtPersona);
+            this.Controls.Add(this.cbPersona);
+            this.Controls.Add(this.btnPersona);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPersona;
+        private System.Windows.Forms.ComboBox cbPersona;
+        private System.Windows.Forms.TextBox txtPersona;
+    }
+}
+
